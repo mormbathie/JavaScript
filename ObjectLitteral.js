a1a3bbbf822f995272a954eda5d2dd897e93b64f
mor mbathie <mormbathie98@gmail.com>
2026-02-11 19:22:52 +0100

    encapsulation de l'utilisateur

---
+++ ObjectLitteral.js
@@ -0,0 +1,16 @@
+const Utilisateur = {
+    name: "mormbathie",
+    age: 25,
+    email: "mormbathie98@gmail.com",
+    amis: ["mohamed", "sami", "yassine", "ayoub"],
+    login(){
+        console.log(`${this.email} est deconnecte`);
+        
+    },
+    logout(){
+        console.log(`${this.email} est deconnecte`);
+    }
+
+}
+
+
