4b342fc4a7eeda90cfe2b1bf4aa56c10346b60ac
mor mbathie <mormbathie98@gmail.com>
2026-02-14 18:35:46 +0100

    heritage

--- class.js
+++ class.js
@@ -16,8 +16,29 @@ class Utilisateur{
 }
 
 
+class Admin extends Utilisateur{
+    constructor(nom, prenom, email, ami, role){
+        super(nom, prenom, email, ami);
+        this.role = role;
+    }
+
+    deleteUser(user){
+        console.log("L'administrateur " + this.prenom + " " + this.nom + " a supprimé l'utilisateur " + user.prenom + " " + user.nom + ".");
+    }
+}
+
 
 var user1 = new Utilisateur("Dupont", "Jean", "jean.dupont@example.com", false);
+var user2 = new Utilisateur("Martin", "Sophie", "sophie.martin@example.com", true);
+var admin1 = new Admin("Durand", "Alice", "alice.durand@example.com", false, "super-admin");
+
+
 console.log(user1);
 user1.login();
 user1.logout();
+console.log(user2);
+user2.login();
+user2.logout();
+console.log(admin1);
+admin1.login();
+admin1.deleteUser(user1);
